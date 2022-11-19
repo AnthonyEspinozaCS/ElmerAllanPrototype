c42d9f60323b3142c43b91e104a237678aeaac57
Anthony Espinoza <aespinoza.cs@gmail.com>
2022-11-18 18:15:07 -0800

    Stylized the BoD (board of directors) page.

--- readMore.js
+++ readMore.js
@@ -1,7 +1,16 @@
-const readMoreBtn = document.querySelector("button.read-more");
-const text = document.getElementById("text-" + readMoreBtn.id);
-
-readMoreBtn.addEventListener("click", () => {
+/* readMoreBtn.addEventListener("click", () => {
     text.classList.toggle("hidden");
     text.classList.toggle("block");
-});
+}); */
+
+function more(person){
+    let personText = document.querySelector(`.${person}-text`);
+    let btnText = document.querySelector(`.read-more-${person}`);
+
+    personText.classList.toggle("hidden");
+    personText.classList.toggle("block");
+    if(btnText.textContent === "Read More")
+        btnText.textContent = "Read Less";
+    else
+        btnText.textContent = "Read More";
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
         'dark-blue': '#00BEBE',
         'gray': '#1B263B',
         'black-custom': '#0D1B2A',
-        'brown-exec': '#A67758',
-        'brown-exec-op': 'rgb(166 119 88 / 0.8)',
+        'brown-exec': '#B99772',
+        'brown-exec-op': '#E4CDB4',
         'white-exec': '#F9F5F2',
       }
     },
